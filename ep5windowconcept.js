deabdbe9d73afa7c35472674152d92edd88d98f1
AmritShekhar <amritshekharrana@gmail.com>
2023-10-30 19:31:43 +0530

    Episode #5: Global Object named Window and this keyword/variable

---
+++ ep5windowconcept.js
@@ -0,0 +1,19 @@
+
+var x = 10;
+
+function a() {
+
+    console.log("This function is named 'a'");
+}
+
+console.log(window.x);
+
+console.log(window.a());
+
+console.log(window.a);
+
+console.log(this.x);
+
+console.log(this.a);
+
+console.log(this.a());
