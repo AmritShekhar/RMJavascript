b6ef9efb842ab0d680c724a66ffe9511526d1a80
AmritShekhar <amritshekharrana@gmail.com>
2023-10-30 18:23:08 +0530

    ep3

---
+++ ep3.js
@@ -0,0 +1,33 @@
+
+
+getName();
+
+console.log(x);
+
+console.log(getName);
+
+var x = 7;
+
+function getName() {
+
+    console.log("Namaste Javascript");
+}
+
+console.log(greeting);
+
+console.log(message);
+
+var greeting = () => {
+
+    console.log("Hello");
+}
+
+
+var message = function() {
+
+    console.log("This program is testing different ways to write functions and how they behave with relation to hoisting in Javascript");
+}
+
+greeting();
+
+message();
